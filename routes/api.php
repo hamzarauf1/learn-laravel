8d69764ab5acb36c4cee3d88055bfde1ea47797b
Hamza Rauf <hamzamian1@gmail.com>
2026-01-04 16:26:44 +0500

    feat: laravel breeze and auth

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,16 @@ use App\Http\Controllers\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('/hello', function (){
-    return ["Message" => "Hello world"];
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::prefix('v1')->group(function(){
+        Route::apiResource('/posts', PostController::class);
+    });
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    }); 
 });
 
-Route::prefix('v1')->group(function(){
-    Route::apiResource('/posts', PostController::class);
-});
+
+
+require __DIR__.'/auth.php';
